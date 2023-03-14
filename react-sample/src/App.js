b3dc1d99cc67f935ff3d5545914f746e1512d82b
Ivan <13834593+ivan-ilyin@users.noreply.github.com>
2023-03-14 18:27:58 -0400

    removed rotating pic and provided email link

--- react-sample/src/App.js
+++ react-sample/src/App.js
@@ -1,16 +1,19 @@
 import headshot from './headshot.jfif';
 import './App.css';
 
+const emailAddress = "ilyin.ivan@gmail.com";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h2>You have reached the personal website of</h2>
         <h1>Ivan Ilyin</h1>
-        <img src={headshot} className="App-logo" alt="headshot of me" />
+        <img src={headshot} alt="headshot of me" />
         <a href="/Ivan-Ilyin-Resume-MAR21-webdev.pdf">
           Open my resume
         </a>
+        <p>Please leave a message in <a href="mailto:{emailAddress}">my email</a></p>
         <p>Or visit my <a href="https://www.linkedin.com/in/ivan-ilyin">LinkedIn page</a></p>
       </header>
     </div>
