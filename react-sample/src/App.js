561867cb01bef3b9e334c211abfafd7a7e4da2df
Ivan <13834593+ivan-ilyin@users.noreply.github.com>
2023-03-14 18:25:25 -0400

    content updates to text

--- react-sample/src/App.js
+++ react-sample/src/App.js
@@ -1,15 +1,17 @@
-import logo from './logo.svg';
+import headshot from './headshot.jfif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>here's my app, just to be clear</p>
+        <h2>You have reached the personal website of</h2>
+        <h1>Ivan Ilyin</h1>
+        <img src={headshot} className="App-logo" alt="headshot of me" />
+        <a href="/Ivan-Ilyin-Resume-MAR21-webdev.pdf">
+          Open my resume
+        </a>
+        <p>Or visit my <a href="https://www.linkedin.com/in/ivan-ilyin">LinkedIn page</a></p>
       </header>
     </div>
   );
