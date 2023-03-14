3033c45f7750134aecf4895efa782b175a270b79
Ivan <13834593+ivan-ilyin@users.noreply.github.com>
2023-03-14 18:30:49 -0400

    css cleanup

--- react-sample/src/App.js
+++ react-sample/src/App.js
@@ -9,12 +9,12 @@ function App() {
       <header className="App-header">
         <h2>You have reached the personal website of</h2>
         <h1>Ivan Ilyin</h1>
-        <img src={headshot} alt="headshot of me" />
-        <a href="/Ivan-Ilyin-Resume-MAR21-webdev.pdf">
+        <img class="Headshot" src={headshot} alt="headshot of me" />
+        <a class="Main-link" href="/Ivan-Ilyin-Resume-MAR21-webdev.pdf">
           Open my resume
         </a>
-        <p>Please leave a message in <a href="mailto:{emailAddress}">my email</a></p>
-        <p>Or visit my <a href="https://www.linkedin.com/in/ivan-ilyin">LinkedIn page</a></p>
+        <p>Please leave a message in <a class="Main-link" href="mailto:{emailAddress}">my email</a></p>
+        <p>Or visit my <a class="Main-link" href="https://www.linkedin.com/in/ivan-ilyin">LinkedIn page</a></p>
       </header>
     </div>
   );
