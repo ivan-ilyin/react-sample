55b6ba2c794b4889e0f1d8631b39baaf2e588a92
Ivan <13834593+ivan-ilyin@users.noreply.github.com>
2023-03-22 15:44:45 -0400

    updated email link

--- react-sample/src/App.js
+++ react-sample/src/App.js
@@ -1,19 +1,19 @@
 import headshot from './headshot.jfif';
 import './App.css';
 
-const emailAddress = "ilyin.ivan@gmail.com";
 
 function App() {
+  const emailAddress = "ilyin.ivan@gmail.com";
   return (
     <div className="App">
       <header className="App-header">
         <h2>You have reached the personal website of</h2>
         <h1>Ivan Ilyin</h1>
         <img class="Headshot" src={headshot} alt="headshot of me" />
-        <a class="Main-link" href="/Ivan-Ilyin-Resume-MAR21-webdev.pdf">
+        <button class="Main-link" href="/Ivan-Ilyin-Resume-MAR21-webdev.pdf">
           Open my resume
-        </a>
-        <p>Please leave a message in <a class="Main-link" href="mailto:{emailAddress}">my email</a></p>
+        </button>
+        <p>Please leave a message in <a class="Main-link" href={"mailto:" + {emailAddress}}>my email</a></p>
         <p>Or visit my <a class="Main-link" href="https://www.linkedin.com/in/ivan-ilyin">LinkedIn page</a></p>
       </header>
     </div>
