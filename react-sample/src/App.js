c81227363e0851803c3aad43a63cf6a454c65642
Ivan <13834593+ivan-ilyin@users.noreply.github.com>
2023-03-22 17:32:08 -0400

    fixed up css

--- react-sample/src/App.js
+++ react-sample/src/App.js
@@ -4,18 +4,19 @@ import { Email } from "react-obfuscate-email";
 
 
 function App() {
-  const emailAddress = "ilyin.ivan@gmail.com";
   return (
     <div className="App">
       <header className="App-header">
         <h2>You have reached the personal website of</h2>
         <h1>Ivan Ilyin</h1>
         <img class="Headshot" src={headshot} alt="headshot of me" />
-        <a target="_blank" class="Resume-button" href="/Ivan-Ilyin-Resume-MAR21-webdev.pdf">
-          <button>Open my resume</button>
-        </a>
-        <p>Feel free to contact me at my email: <Email email="ilyin.ivan@gmail.com">ilyin.ivan@gmail.com"</Email></p>
-        <p>Or visit my <a class="Main-link" href="https://www.linkedin.com/in/ivan-ilyin">LinkedIn page</a></p>
+        <div>
+          <a target="_blank" href="/Ivan-Ilyin-Resume-MAR21-webdev.pdf">
+            <button class="Resume-button" >Open my resume</button>
+          </a>
+        </div>
+        <p>Feel free to contact me at my email: <Email class="Inline-link" email="ilyin.ivan@gmail.com">ilyin.ivan@gmail.com</Email></p>
+        <p>Or visit my <a target="_blank" class="Inline-link" href="https://www.linkedin.com/in/ivan-ilyin" rel="noreferrer">LinkedIn page</a></p>
       </header>
     </div>
   );
